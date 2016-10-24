37eca90cb18e9fdbcfe67db8e9f7cbdea752b797
Falko Mierke <winsling@yahoo.de>
2016-10-24 22:13:13 +0200

    new file ue from other raspi

---
+++ ue.py
@@ -0,0 +1,68 @@
+#Bibliotheken einbinden
+import RPi.GPIO as GPIO
+import time
+
+#GPIO Modus (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+#GPIO Pins zuweisen
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+GPIO_LED = 3
+GPIO_PIR = 17
+
+#Richtung der GPIO-Pins festlegen (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_LED,GPIO.OUT)
+GPIO.setup(GPIO_PIR,GPIO.IN)
+
+def distanz():
+	# setze Trigger auf HIGH
+	
+	GPIO.output(GPIO_TRIGGER, True)
+	
+
+	# setze Trigger nach 0.01ms aus LOW
+	time.sleep(0.00001)
+	GPIO.output(GPIO_TRIGGER, False)
+
+	StartZeit = time.time()
+	StopZeit = time.time()
+
+        
+	# speichere Startzeit
+	while GPIO.input(GPIO_ECHO) == 0:
+		StartZeit = time.time()
+        
+	# speichere Ankunftszeit
+	while GPIO.input(GPIO_ECHO) == 1:
+		StopZeit = time.time()
+        
+	# Zeit Differenz zwischen Start und Ankunft
+	TimeElapsed = StopZeit - StartZeit
+	# mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
+	# und durch 2 teilen, da hin und zurueck
+	distanz = (TimeElapsed * 34300) / 2
+	
+
+	return distanz
+
+if __name__ == '__main__':
+	try:
+		time.sleep(2)
+		while True:
+			abstand = distanz()
+			#print ("Gemessene Entfernung = %.1f cm" % abstand)
+			i=GPIO.input(GPIO_PIR)
+			if i==0:
+				GPIO.output(GPIO_LED,0)
+			elif i==1:
+				GPIO.output(GPIO_LED,1)
+				print ("Distance = %.1f cm"% abstand)
+			time.sleep(1)
+
+		# Beim Abbruch durch STRG+C resetten
+	except KeyboardInterrupt:
+		print("Messung vom User gestoppt")
+		GPIO.cleanup()
